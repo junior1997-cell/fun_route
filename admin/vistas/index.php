2a4355a0d5ffe361d704a904d18c48126d618916
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2023-03-29 22:26:51 -0500

    admin

--- admin/vistas/index.php
+++ admin/vistas/index.php
@@ -7,11 +7,7 @@
             $link_host = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'].'/';
         }else{
             if ($tipo == 'local') {
-<<<<<<< HEAD
-                $link_host = "http://localhost/front_sevens/admin/";
-=======
                 $link_host = "http://localhost/page_amazon_lab/admin/";
->>>>>>> cae41abf91959a0e6fc33cf97519c0cb99f6595e
             }            
         }
         return $link_host;
