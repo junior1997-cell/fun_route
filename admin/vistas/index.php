11faab47535fae5b95734db132b2246a143d8c1d
Junior Cercado Vasquez <juniorcercado@upeu.edu.pe>
2023-08-25 00:35:43 -0500

    casi completo, subid a la nube par aprueba

--- admin/vistas/index.php
+++ admin/vistas/index.php
@@ -4,16 +4,16 @@
 
     function enrutamiento($tipo, $file) {
         if ($tipo == 'nube') {
-            $link_host = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'].'/vistas/login.html?file='.$file;
+            $link_host = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'].'/admin/vistas/login.html?file='.$file;
         }else{
             if ($tipo == 'local') {
-                $link_host = "http://localhost/fun_route/admin/vistas/login.html?file=".$file;
+                $link_host = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']."/fun_route/admin/vistas/login.html?file=".$file;
             }            
         }
         return $link_host;
     }
 
-    if ($_SERVER['HTTP_HOST'] == "localhost") {
+    if ($_SERVER['HTTP_HOST'] == "localhost" || floatval($_SERVER['SERVER_NAME']) > 0 ) {
         $ruta = enrutamiento('local', $file_go);
     } else {
         $ruta = enrutamiento('nube', $file_go);
